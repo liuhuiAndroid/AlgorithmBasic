7868d19bd45c1de5d9521a84e33c0b0308aa2faa
lh <im.lh@hotmail.com>
2022-03-01 12:25:31 +0800

    二叉树基本算法和贪心算法

--- src/class13/Code01_IsCBT.java
+++ src/class13/Code01_IsCBT.java
@@ -2,116 +2,130 @@ package class13;
 
 import java.util.LinkedList;
 
+/**
+ * 是否是完全二叉树
+ * 1。左满 右满 左高 == 右高
+ * 2。左完 右满 左高 == 右高 + 1
+ * 3。左满 右满 左高 == 右高 + 1
+ * 4。左满 右完 左高 == 右高
+ */
 public class Code01_IsCBT {
 
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
 
-		public Node(int data) {
-			this.value = data;
-		}
-	}
+        public Node(int data) {
+            this.value = data;
+        }
+    }
 
-	public static boolean isCBT1(Node head) {
-		if (head == null) {
-			return true;
-		}
-		LinkedList<Node> queue = new LinkedList<>();
-		// 是否遇到过左右两个孩子不双全的节点
-		boolean leaf = false;
-		Node l = null;
-		Node r = null;
-		queue.add(head);
-		while (!queue.isEmpty()) {
-			head = queue.poll();
-			l = head.left;
-			r = head.right;
-			if (
-			// 如果遇到了不双全的节点之后，又发现当前节点不是叶节点
-			(leaf && (l != null || r != null)) || (l == null && r != null)
+    public static boolean isCBT1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        LinkedList<Node> queue = new LinkedList<>();
+        // 是否遇到过左右两个孩子不双全的节点
+        boolean leaf = false;
+        Node l = null;
+        Node r = null;
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            l = head.left;
+            r = head.right;
+            if (
+                // 如果遇到了不双全的节点之后，又发现当前节点不是叶节点
+                    (leaf && (l != null || r != null)) || (l == null && r != null)
 
-			) {
-				return false;
-			}
-			if (l != null) {
-				queue.add(l);
-			}
-			if (r != null) {
-				queue.add(r);
-			}
-			if (l == null || r == null) {
-				leaf = true;
-			}
-		}
-		return true;
-	}
+            ) {
+                return false;
+            }
+            if (l != null) {
+                queue.add(l);
+            }
+            if (r != null) {
+                queue.add(r);
+            }
+            if (l == null || r == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
 
-	public static boolean isCBT2(Node head) {
-		return process(head).isCBT;
-	}
+    public static boolean isCBT2(Node head) {
+        return process(head).isCBT;
+    }
 
-	public static class Info {
-		public boolean isFull;
-		public boolean isCBT;
-		public int height;
+    public static class Info {
+        // 是否是满的
+        public boolean isFull;
+        // 是否是完全二叉树
+        public boolean isCBT;
+        // 高度
+        public int height;
 
-		public Info(boolean full, boolean cbt, int h) {
-			isFull = full;
-			isCBT = cbt;
-			height = h;
-		}
-	}
+        public Info(boolean full, boolean cbt, int h) {
+            isFull = full;
+            isCBT = cbt;
+            height = h;
+        }
+    }
 
-	public static Info process(Node x) {
-		if (x == null) {
-			return new Info(true, true, 0);
-		}
-		Info leftInfo = process(x.left);
-		Info rightInfo = process(x.right);
-		int height = Math.max(leftInfo.height, rightInfo.height) + 1;
-		boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
-		boolean isCBT = false;
-		if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height) {
-			isCBT = true;
-		} else if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
-			isCBT = true;
-		} else if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
-			isCBT = true;
-		} else if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
-			isCBT = true;
-		}
-		return new Info(isFull, isCBT, height);
-	}
+    /**
+     * 以x节点为头
+     */
+    public static Info process(Node x) {
+        // 空数可以认为是满二叉树、完全二叉树，高度为0
+        if (x == null) {
+            return new Info(true, true, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
+        boolean isCBT = false;
+        if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        } else if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        }
+        return new Info(isFull, isCBT, height);
+    }
 
-	// for test
-	public static Node generateRandomBST(int maxLevel, int maxValue) {
-		return generate(1, maxLevel, maxValue);
-	}
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
 
-	// for test
-	public static Node generate(int level, int maxLevel, int maxValue) {
-		if (level > maxLevel || Math.random() < 0.5) {
-			return null;
-		}
-		Node head = new Node((int) (Math.random() * maxValue));
-		head.left = generate(level + 1, maxLevel, maxValue);
-		head.right = generate(level + 1, maxLevel, maxValue);
-		return head;
-	}
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
 
-	public static void main(String[] args) {
-		int maxLevel = 5;
-		int maxValue = 100;
-		int testTimes = 1000000;
-		for (int i = 0; i < testTimes; i++) {
-			Node head = generateRandomBST(maxLevel, maxValue);
-			if (isCBT1(head) != isCBT2(head)) {
-				System.out.println("Oops!");
-			}
-		}
-		System.out.println("finish!");
-	}
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isCBT1(head) != isCBT2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
 
 }
