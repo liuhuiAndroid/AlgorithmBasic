40dc10d6750916127c040596251bbebcb5584dce
lh <im.lh@hotmail.com>
2022-03-21 13:32:16 +0800

    排序总结、链表相关面试题

--- src/class09/Code01_LinkedListMid.java
+++ src/class09/Code01_LinkedListMid.java
@@ -2,161 +2,180 @@ package class09;
 
 import java.util.ArrayList;
 
+/**
+ * 排序算法总结
+ * 1：不基于比较的排序，对样本数据有严格要求，不易改写
+ * 2。基于比较的排序，只要规定好两个样本怎么比大小就可以直接复用
+ * 3。基于比较的排序，时间复杂度的极限是O(N*logN)：归并、快排和堆，额外空间复杂度O(N)O(logN)O(1)，归并稳定，快排常数时间好，堆排序空间好
+ * 4。时间复杂度O(N*logN)、额外空间复杂度低于O(N)、且稳定的基于比较的排序是不存在的
+ * 5。为了绝对的速度选快排、为了省空间选堆排、为了稳定性选归并*
+ * <p>
+ * 工程上对排序的改进
+ * 1。稳定性的考虑
+ * 2。充分利用O(N*logN)和O(N^2)排序各自的优势
+ * <p>
+ * 链表问题
+ * 面试时链表解题的方法论
+ * 1：对于笔试，不用太在乎空间复杂度，一切为了时间复杂度
+ * 2。对于面试，时间复杂度依然放在第一位，但是一定要找到空间最省的方法
+ * <p>
+ * 找到链表的中点位置
+ * 快慢指针法：快指针一次走两步，慢指针一次走一步
+ */
 public class Code01_LinkedListMid {
 
-	public static class Node {
-		public int value;
-		public Node next;
+    public static class Node {
+        public int value;
+        public Node next;
 
-		public Node(int v) {
-			value = v;
-		}
-	}
+        public Node(int v) {
+            value = v;
+        }
+    }
 
-	// head 头
-	public static Node midOrUpMidNode(Node head) {
-		if (head == null || head.next == null || head.next.next == null) {
-			return head;
-		}
-		// 链表有3个点或以上
-		Node slow = head.next;
-		Node fast = head.next.next;
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		return slow;
-	}
+    // head 头
+    public static Node midOrUpMidNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        // 链表有3个点或以上
+        Node slow = head.next;
+        Node fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
-	public static Node midOrDownMidNode(Node head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-		Node slow = head.next;
-		Node fast = head.next;
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		return slow;
-	}
+    public static Node midOrDownMidNode(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node slow = head.next;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
-	public static Node midOrUpMidPreNode(Node head) {
-		if (head == null || head.next == null || head.next.next == null) {
-			return null;
-		}
-		Node slow = head;
-		Node fast = head.next.next;
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		return slow;
-	}
+    public static Node midOrUpMidPreNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
-	public static Node midOrDownMidPreNode(Node head) {
-		if (head == null || head.next == null) {
-			return null;
-		}
-		if (head.next.next == null) {
-			return head;
-		}
-		Node slow = head;
-		Node fast = head.next;
-		while (fast.next != null && fast.next.next != null) {
-			slow = slow.next;
-			fast = fast.next.next;
-		}
-		return slow;
-	}
+    public static Node midOrDownMidPreNode(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        if (head.next.next == null) {
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
-	public static Node right1(Node head) {
-		if (head == null) {
-			return null;
-		}
-		Node cur = head;
-		ArrayList<Node> arr = new ArrayList<>();
-		while (cur != null) {
-			arr.add(cur);
-			cur = cur.next;
-		}
-		return arr.get((arr.size() - 1) / 2);
-	}
+    public static Node right1(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 1) / 2);
+    }
 
-	public static Node right2(Node head) {
-		if (head == null) {
-			return null;
-		}
-		Node cur = head;
-		ArrayList<Node> arr = new ArrayList<>();
-		while (cur != null) {
-			arr.add(cur);
-			cur = cur.next;
-		}
-		return arr.get(arr.size() / 2);
-	}
+    public static Node right2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get(arr.size() / 2);
+    }
 
-	public static Node right3(Node head) {
-		if (head == null || head.next == null || head.next.next == null) {
-			return null;
-		}
-		Node cur = head;
-		ArrayList<Node> arr = new ArrayList<>();
-		while (cur != null) {
-			arr.add(cur);
-			cur = cur.next;
-		}
-		return arr.get((arr.size() - 3) / 2);
-	}
+    public static Node right3(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 3) / 2);
+    }
 
-	public static Node right4(Node head) {
-		if (head == null || head.next == null) {
-			return null;
-		}
-		Node cur = head;
-		ArrayList<Node> arr = new ArrayList<>();
-		while (cur != null) {
-			arr.add(cur);
-			cur = cur.next;
-		}
-		return arr.get((arr.size() - 2) / 2);
-	}
+    public static Node right4(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 2) / 2);
+    }
 
-	public static void main(String[] args) {
-		Node test = null;
-		test = new Node(0);
-		test.next = new Node(1);
-		test.next.next = new Node(2);
-		test.next.next.next = new Node(3);
-		test.next.next.next.next = new Node(4);
-		test.next.next.next.next.next = new Node(5);
-		test.next.next.next.next.next.next = new Node(6);
-		test.next.next.next.next.next.next.next = new Node(7);
-		test.next.next.next.next.next.next.next.next = new Node(8);
+    public static void main(String[] args) {
+        Node test = null;
+        test = new Node(0);
+        test.next = new Node(1);
+        test.next.next = new Node(2);
+        test.next.next.next = new Node(3);
+        test.next.next.next.next = new Node(4);
+        test.next.next.next.next.next = new Node(5);
+        test.next.next.next.next.next.next = new Node(6);
+        test.next.next.next.next.next.next.next = new Node(7);
+        test.next.next.next.next.next.next.next.next = new Node(8);
 
-		Node ans1 = null;
-		Node ans2 = null;
+        Node ans1 = null;
+        Node ans2 = null;
 
-		ans1 = midOrUpMidNode(test);
-		ans2 = right1(test);
-		System.out.println(ans1 != null ? ans1.value : "无");
-		System.out.println(ans2 != null ? ans2.value : "无");
+        ans1 = midOrUpMidNode(test);
+        ans2 = right1(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
 
-		ans1 = midOrDownMidNode(test);
-		ans2 = right2(test);
-		System.out.println(ans1 != null ? ans1.value : "无");
-		System.out.println(ans2 != null ? ans2.value : "无");
+        ans1 = midOrDownMidNode(test);
+        ans2 = right2(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
 
-		ans1 = midOrUpMidPreNode(test);
-		ans2 = right3(test);
-		System.out.println(ans1 != null ? ans1.value : "无");
-		System.out.println(ans2 != null ? ans2.value : "无");
+        ans1 = midOrUpMidPreNode(test);
+        ans2 = right3(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
 
-		ans1 = midOrDownMidPreNode(test);
-		ans2 = right4(test);
-		System.out.println(ans1 != null ? ans1.value : "无");
-		System.out.println(ans2 != null ? ans2.value : "无");
-
-	}
+        ans1 = midOrDownMidPreNode(test);
+        ans2 = right4(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
+    }
 
 }
