c12600a04a1e2051ce55ff71c193708a0b41304c
lh <im.lh@hotmail.com>
2022-04-11 22:37:42 +0800

    Morris遍历

--- src/class30/Code01_MorrisTraversal.java
+++ src/class30/Code01_MorrisTraversal.java
@@ -4,247 +4,247 @@ package class30;
  * 二叉树的Morris遍历
  * 二叉树之前的遍历方式有空间浪费的问题，优化空间复杂度
  * Morris遍历时间复杂度O(N)，额外空间复杂度O(1)，通过利用原树中大量空闲指针的方式，达到节省空间的目的
- *
+ * <p>
  * 假设来到当前节点cur，开始时cur来到头节点位置
  * 1）如果cur没有左孩子，cur向右移动(cur = cur.right)
  * 2）如果cur有左孩子，找到左子树上最右的节点mostRight：
- * 		a.如果mostRight的右指针指向空，让其指向cur，
- * 		然后cur向左移动(cur = cur.left)
- * 		b.如果mostRight的右指针指向cur，让其指向null，
- * 		然后cur向右移动(cur = cur.right)
+ * a.如果mostRight的右指针指向空，让其指向cur，
+ * 然后cur向左移动(cur = cur.left)
+ * b.如果mostRight的右指针指向cur，让其指向null，
+ * 然后cur向右移动(cur = cur.right)
  * 3）cur为空时遍历停止
- *
+ * <p>
  * Morris遍历实现二叉树的先序、中序、后序遍历
- *
+ * <p>
  * 题目：
  * Morris遍历的实现
  * 给定一棵二叉树的头节点head，求以head为头的树中，最小深度是多少？
  */
 public class Code01_MorrisTraversal {
 
-	public static class Node {
-		public int value;
-		Node left;
-		Node right;
+    public static class Node {
+        public int value;
+        Node left;
+        Node right;
 
-		public Node(int data) {
-			this.value = data;
-		}
-	}
+        public Node(int data) {
+            this.value = data;
+        }
+    }
 
-	public static void process(Node root) {
-		if (root == null) {
-			return;
-		}
-		// 1
-		process(root.left);
-		// 2
-		process(root.right);
-		// 3
-	}
+    public static void process(Node root) {
+        if (root == null) {
+            return;
+        }
+        // 1
+        process(root.left);
+        // 2
+        process(root.right);
+        // 3
+    }
 
-	public static void morris(Node head) {
-		if (head == null) {
-			return;
-		}
-		Node cur = head;
-		Node mostRight = null;
-		while (cur != null) {
-			mostRight = cur.left;
-			if (mostRight != null) {
-				while (mostRight.right != null && mostRight.right != cur) {
-					mostRight = mostRight.right;
-				}
-				if (mostRight.right == null) {
-					mostRight.right = cur;
-					cur = cur.left;
-					continue;
-				} else {
-					mostRight.right = null;
-				}
-			}
-			cur = cur.right;
-		}
-	}
+    public static void morris(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            cur = cur.right;
+        }
+    }
 
-	public static void morrisPre(Node head) {
-		if (head == null) {
-			return;
-		}
-		Node cur = head;
-		Node mostRight = null;
-		while (cur != null) {
-			mostRight = cur.left;
-			if (mostRight != null) {
-				while (mostRight.right != null && mostRight.right != cur) {
-					mostRight = mostRight.right;
-				}
-				if (mostRight.right == null) {
-					System.out.print(cur.value + " ");
-					mostRight.right = cur;
-					cur = cur.left;
-					continue;
-				} else {
-					mostRight.right = null;
-				}
-			} else {
-				System.out.print(cur.value + " ");
-			}
-			cur = cur.right;
-		}
-		System.out.println();
-	}
+    public static void morrisPre(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    System.out.print(cur.value + " ");
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            } else {
+                System.out.print(cur.value + " ");
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
 
-	public static void morrisIn(Node head) {
-		if (head == null) {
-			return;
-		}
-		Node cur = head;
-		Node mostRight = null;
-		while (cur != null) {
-			mostRight = cur.left;
-			if (mostRight != null) {
-				while (mostRight.right != null && mostRight.right != cur) {
-					mostRight = mostRight.right;
-				}
-				if (mostRight.right == null) {
-					mostRight.right = cur;
-					cur = cur.left;
-					continue;
-				} else {
-					mostRight.right = null;
-				}
-			}
-			System.out.print(cur.value + " ");
-			cur = cur.right;
-		}
-		System.out.println();
-	}
+    public static void morrisIn(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            System.out.print(cur.value + " ");
+            cur = cur.right;
+        }
+        System.out.println();
+    }
 
-	public static void morrisPos(Node head) {
-		if (head == null) {
-			return;
-		}
-		Node cur = head;
-		Node mostRight = null;
-		while (cur != null) {
-			mostRight = cur.left;
-			if (mostRight != null) {
-				while (mostRight.right != null && mostRight.right != cur) {
-					mostRight = mostRight.right;
-				}
-				if (mostRight.right == null) {
-					mostRight.right = cur;
-					cur = cur.left;
-					continue;
-				} else {
-					mostRight.right = null;
-					printEdge(cur.left);
-				}
-			}
-			cur = cur.right;
-		}
-		printEdge(head);
-		System.out.println();
-	}
+    public static void morrisPos(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printEdge(head);
+        System.out.println();
+    }
 
-	public static void printEdge(Node head) {
-		Node tail = reverseEdge(head);
-		Node cur = tail;
-		while (cur != null) {
-			System.out.print(cur.value + " ");
-			cur = cur.right;
-		}
-		reverseEdge(tail);
-	}
+    public static void printEdge(Node head) {
+        Node tail = reverseEdge(head);
+        Node cur = tail;
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.right;
+        }
+        reverseEdge(tail);
+    }
 
-	public static Node reverseEdge(Node from) {
-		Node pre = null;
-		Node next = null;
-		while (from != null) {
-			next = from.right;
-			from.right = pre;
-			pre = from;
-			from = next;
-		}
-		return pre;
-	}
+    public static Node reverseEdge(Node from) {
+        Node pre = null;
+        Node next = null;
+        while (from != null) {
+            next = from.right;
+            from.right = pre;
+            pre = from;
+            from = next;
+        }
+        return pre;
+    }
 
-	// for test -- print tree
-	public static void printTree(Node head) {
-		System.out.println("Binary Tree:");
-		printInOrder(head, 0, "H", 17);
-		System.out.println();
-	}
+    // for test -- print tree
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
 
-	public static void printInOrder(Node head, int height, String to, int len) {
-		if (head == null) {
-			return;
-		}
-		printInOrder(head.right, height + 1, "v", len);
-		String val = to + head.value + to;
-		int lenM = val.length();
-		int lenL = (len - lenM) / 2;
-		int lenR = len - lenM - lenL;
-		val = getSpace(lenL) + val + getSpace(lenR);
-		System.out.println(getSpace(height * len) + val);
-		printInOrder(head.left, height + 1, "^", len);
-	}
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
 
-	public static String getSpace(int num) {
-		String space = " ";
-		StringBuffer buf = new StringBuffer("");
-		for (int i = 0; i < num; i++) {
-			buf.append(space);
-		}
-		return buf.toString();
-	}
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
 
-	public static boolean isBST(Node head) {
-		if (head == null) {
-			return true;
-		}
-		Node cur = head;
-		Node mostRight = null;
-		Integer pre = null;
-		boolean ans = true;
-		while (cur != null) {
-			mostRight = cur.left;
-			if (mostRight != null) {
-				while (mostRight.right != null && mostRight.right != cur) {
-					mostRight = mostRight.right;
-				}
-				if (mostRight.right == null) {
-					mostRight.right = cur;
-					cur = cur.left;
-					continue;
-				} else {
-					mostRight.right = null;
-				}
-			}
-			if (pre != null && pre >= cur.value) {
-				ans = false;
-			}
-			pre = cur.value;
-			cur = cur.right;
-		}
-		return ans;
-	}
+    public static boolean isBST(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        Integer pre = null;
+        boolean ans = true;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            if (pre != null && pre >= cur.value) {
+                ans = false;
+            }
+            pre = cur.value;
+            cur = cur.right;
+        }
+        return ans;
+    }
 
-	public static void main(String[] args) {
-		Node head = new Node(4);
-		head.left = new Node(2);
-		head.right = new Node(6);
-		head.left.left = new Node(1);
-		head.left.right = new Node(3);
-		head.right.left = new Node(5);
-		head.right.right = new Node(7);
-		printTree(head);
-		morrisIn(head);
-		morrisPre(head);
-		morrisPos(head);
-		printTree(head);
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(3);
+        head.right.left = new Node(5);
+        head.right.right = new Node(7);
+        printTree(head);
+        morrisIn(head);
+        morrisPre(head);
+        morrisPos(head);
+        printTree(head);
 
-	}
+    }
 
 }
